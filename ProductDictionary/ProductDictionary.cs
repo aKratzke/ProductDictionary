f72b90a1c56d3cd450275197550101135857824a
aKratzke <arosekratzke@gmail.com>
2015-07-19 20:13:58 -0400

    Product Dictionary
    Builds a Dictionary based upon user input and then displays items in
    said Dictionary to user.

---
+++ ProductDictionary/ProductDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductDictionary
+{
+    class Product
+    {
+        //Initialize all varaibles I want to use throughout the class
+
+        string pName;
+        double pCost;
+        string pManufacturer;
+
+
+        public Product()            //Plug in some values for our Constructor
+        { }
+
+        //Create Method to hold information with product cost and the product manufacturer
+        public Product(string pN, double pC, string pM)
+        {
+            pName = pN;
+            pCost = pC;
+            pManufacturer = pM;
+        }
+
+        public void printItemName()
+        {
+            Console.WriteLine("\nProduct Name: {0}\n", pName);
+            Console.WriteLine("Product Cost: {0}", pCost);
+            Console.WriteLine("Product Manufacturer: {0}", pManufacturer);
+        }
+    }
+}
---
+++ ProductDictionary/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            buildDictionary();
+        }
+
+        static void buildDictionary()
+        {
+            int x;
+
+            string pName;
+            double pCost;
+            string pManufacturer;
+
+            Dictionary<string, Product> productCatalog = new Dictionary<string, Product>();
+
+            
+
+            do
+            {
+                Console.WriteLine("Please enter the name of a product you wish to add to the dictionary.");
+                pName = Console.ReadLine();
+
+                Console.WriteLine("What is the cost of this product?");
+                pCost = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Who manufactures this product?");
+                pManufacturer = Console.ReadLine();
+
+                Product newProduct = new Product(pName, pCost, pManufacturer);
+                
+                productCatalog.Add(pName, newProduct);
+
+                Console.WriteLine("Would you like to enter another product, or view the product catalog?");
+
+                Console.WriteLine("Press 1 to enter another product or 2 to view the product catalog");
+                x = Int32.Parse(Console.ReadLine());
+            } while (x != 2);
+
+
+            foreach (KeyValuePair<string, Product> item in productCatalog)
+            {
+                item.Value.printItemName();
+            }
+            
+        }
+    }
+}
